5036ba6f320d78e1787274cb8bdca3fb4d855f17
ruchisharmaku-debug <ruchisharmaku@gmail.com>
2026-02-04 20:45:01 +0530

    Add files via upload

---
+++ main (31).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    
+    cout<<"printing cout from 1 to n"<< endl;
+  /*    int i = 1;
+    for(; ;){
+        if (i<=n) {
+            cout<< i << endl;
+        }
+        else{
+            break;
+        }
+        i++;
+    }
+    */
+    for(int a = 5 , b = 6, c=7; a>=0 && b>=1 && c>=2; a--, b--,c--){
+        cout<<a <<" "<<b<<"  " <<c << endl;
+    }
+      
+}
